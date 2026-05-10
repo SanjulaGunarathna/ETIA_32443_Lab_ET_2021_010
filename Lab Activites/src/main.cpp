c6f9bf74d268dfed7d7958f3117450db373cb894
Sanjula Gunarathna <sanjulagunarathna.307@gmail.com>
2026-05-10 12:41:09 +0530

    Activity_01

---
+++ Lab Activites/src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+
+    DDRB |= (1 << DDB0);
+
+
+    while (1)
+    {
+
+        PORTB |= (1 << PORTB0);
+        _delay_ms(1000);
+
+        PORTB &= ~(1 << PORTB0);
+        _delay_ms(1000);
+    }
+}
